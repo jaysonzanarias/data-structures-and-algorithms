434992cff02caf8c54b9f93d536bf41245e4f3ad
jaysonzanarias <jayson.zanarias@gmail.com>
2020-06-08 01:31:46 +0800

    Added Merge Sort, Binary Search, and updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# data_structures_and_algorithms
-Data Structures and Algorithms
+# Data Structures and Algorithms
+My personal reviewer for different Data Structure and Algorithm Concepts.
+<br><br>
+**Note:** 
+- items listed are not complete, only the ones I was only able to study at the moment. Will continue to update.
+- for more details, you may visit this site https://www.bigocheatsheet.com/, it listed a lot of data structures and algorithms together with it's time complexity + a link to Wikipedia for each item.
+
+## Language Used
+- Java
+
+## Data Structures
+- Array
+- Stack
+- Queue
+- LinkedList
+- Tree
+
+## Sorting Algorithms
+- Bubble Sort
+- Selection Sort
+- Insertion Sort
+- Merge Sort
+
+## Time Complexity
+- Data Structures
+
+    | Data Structure | Access | Search | Insert | Delete |
+    |      ---       |  ---   |  ---   |  ---   |  ---   |
+    | Array          |  O(1)  |  O(n)  |  O(n)  |  O(n)  |
+    | Stack          |  O(n)  |  O(n)  |  O(1)  |  O(1)  |
+    | Queue          |  O(n)  |  O(n)  |  O(1)  |  O(1)  |
+    | LinkedList     |  O(n)  |  O(n)  |  O(1)  |  O(1)  |
+    | Tree           | O(log<sub>2</sub>n) | O(log<sub>2</sub>n) | O(log<sub>2</sub>n) | O(log<sub>2</sub>n)  |
+
+## Tree
+
+### Tree Traversal
+- In-Order
+- Pre-Order
+- Post-Order
+
+### Unbalanced vs Balanced Trees
+- Unbalanced Tree
+    - All of the weight of tree is on one side.
+- Balanced
+    - Example: AVL Trees, Red-Black Trees, B-Trees, etc.
+
+## References
+- https://www.linkedin.com/learning/introduction-to-data-structures-algorithms-in-java
+- https://www.tutorialspoint.com/data_structures_algorithms/
+- https://stackoverflow.com/questions/487258/what-is-a-plain-english-explanation-of-big-o-notation
+- https://www.bigocheatsheet.com/
---
+++ src/main/java/searching/binary_search/TreeNode.java
@@ -0,0 +1,98 @@
+package searching.binary_search;
+
+public class TreeNode {
+    private Integer data;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+    private boolean isDeleted = false;
+
+    public TreeNode(Integer data) {
+        this.data = data;
+    }
+
+    public Integer smallest() {
+        if (leftChild == null) {
+            return data;
+        } else {
+            return leftChild.smallest();
+        }
+    }
+
+    public Integer largest() {
+        if (rightChild == null) {
+            return data;
+        } else {
+            return rightChild.smallest();
+        }
+    }
+
+    public void traverseInOrder() {
+        if (this.leftChild != null) {
+            this.leftChild.traverseInOrder();
+        }
+        System.out.print(this + " ");
+        if (this.rightChild != null) {
+            this.rightChild.traverseInOrder();
+        }
+    }
+
+    public TreeNode find(Integer data) {
+        if (this.data == data && !isDeleted) {
+            return this;
+        }
+
+        if (data < this.data && leftChild != null) {
+            return leftChild.find(data);
+        }
+
+        if (rightChild != null) {
+            return rightChild.find(data);
+        }
+
+        return null;
+    }
+
+    public void insert(Integer data) {
+        if (data >= this.data) {
+            if (rightChild == null) {
+                rightChild = new TreeNode(data);
+            } else {
+                rightChild.insert(data);
+            }
+        } else {
+            if (leftChild == null) {
+                leftChild = new TreeNode(data);
+            } else {
+                leftChild.insert(data);
+            }
+        }
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public void delete() {
+        isDeleted = true;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+}
